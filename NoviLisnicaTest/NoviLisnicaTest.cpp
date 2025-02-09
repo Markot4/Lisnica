57b93a8555de455d98337fb313c03ddef134d567
Marko Šeparović <maseparov@vsite.hr>
2025-02-09 20:45:57 +0100

    U test projektu usvojio primjedbe na postavke za linker.

--- NoviLisnicaTest/NoviLisnicaTest.cpp
+++ NoviLisnicaTest/NoviLisnicaTest.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "Dionica.h"
-#include "Obveznica.h"
-#include "LisnicaClass.h"
+#include "..\Dionica.h"
+#include "..\Obveznica.h"
+#include "..\LisnicaClass.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace markot4;
@@ -27,26 +27,6 @@ namespace LisnicaTest
 			Assert::AreEqual(94.0, o.cijena);
 			std::string ocekivano = "PL-A";
 			Assert::AreEqual(ocekivano, o.oznaka);
-		}
-		
-		TEST_METHOD(TestLisnicaClass)
-		{
-			LisnicaClass lisnica;
-
-			// Dodavanje vrijednosnog papira
-			Dionica d("PL-V-A", 107);
-			lisnica.dodajVrijednosniPapir(d);
-
-			// Provjera dodane dionice
-			Assert::AreEqual(107.0, lisnica.vrijPoVrijPapir("PL-V-A"));
-
-			// Promjena cijene
-			lisnica.promjenaCijene(120.0, "PL-V-A");
-			Assert::AreEqual(120.0, lisnica.vrijPoVrijPapir("PL-V-A"));
-
-			// Izbacivanje papira
-			lisnica.izbaciVrijednosniPapir("PL-V-A");
-			Assert::AreEqual(0.0, lisnica.vrijPoVrijPapir("PL-V-A"));
 		}
 		
 
