a4ae6a17fa94838e7876e0b1546c0f0827c8a780
Marko Šeparović <maseparov@vsite.hr>
2025-03-02 10:01:37 +0100

    dodao jos testova za obveznicu

--- NoviLisnicaTest/NoviLisnicaTest.cpp
+++ NoviLisnicaTest/NoviLisnicaTest.cpp
@@ -38,14 +38,36 @@ namespace LisnicaTest
 			Assert::AreEqual(d.izracunajVrijednost() ,  107 * 12.3);
 		}
 		
-		TEST_METHOD(TestObveznica)
+		TEST_METHOD(TestObveznicaIme)
 		{
 			Obveznica o("PL-A", 94, 5, 12.5);
-			Assert::AreEqual(94.0, o.cijena);
 			std::string ocekivano = "PL-A";
 			Assert::AreEqual(ocekivano, o.oznaka);
 		}
-	
+
+		TEST_METHOD(TestObveznicaCijena)
+		{
+			Obveznica o("PL-A", 94.2, 5, 12.5);
+			Assert::AreEqual(94.2, o.cijena);
+		}
+		
+		TEST_METHOD(TestObveznicaKolicina)
+		{
+			Obveznica o("PL-A", 94, 5, 12.5);
+			Assert::AreEqual(5, o.kolicina);
+		}
+
+		TEST_METHOD(TestObveznicaNominalnaCijena)
+		{
+			Obveznica o("PL-A", 94, 5, 12.5);
+			Assert::AreEqual(12.5, o.nominalnaCijena);
+		}
+
+		TEST_METHOD(TestObveznicaVrijednost)
+		{
+			Obveznica o("PL-A", 94, 5, 12.5);
+			Assert::AreEqual(o.izracunajVrijednost(), 94 * 12.5);
+		}
 	};
 }
 
